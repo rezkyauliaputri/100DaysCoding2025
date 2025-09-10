77d3bb426923e9e1e0b7d66bb3bffbaac2ee40ac
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-10 15:25:58 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,28 @@
+package Day3.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+      int umur = 19;
+        String nama = "Rezky aulia putri";
+        /* yang di atas adalah type data int dan String dengan variabel umur dan nama yang berisi 19,rezky aulia putri*/
+         
+         
+        /* untuk komentar yang memiliki tanda bintang dan garis miring(/*)
+         dan ditutup lagi dengan bintang dan garis miring ini/
+        yaitu untuk komentar 1 baris atau 1 line*/
+        
+        System.out.println(umur);
+        System.out.println(nama);
+        //yang di atas untuk menampilkan hasil setelah di run/di output
+        
+        //dan komen yang memiliki 2 garis miring atau 2 slice untuk komen 1 line/baris
+        
+        
+        
+         
+        
+        
+        
+    }   
+}
