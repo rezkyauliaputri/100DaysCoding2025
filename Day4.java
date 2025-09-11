ff5d1db6b765b23f98a27cb819762f2b7d7cf261
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-11 17:05:54 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,20 @@
+package Day4.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("asslamualaikum\nkakak");
+        System.out.println("Nama saya aulia  Kelas \"c\"");
+        System.out.println("Nim\t:D0225029");
+        System.out.println("umu\br:19");
+        System.out.println("gender \'p\'");
+        
+        //* \n digunakan untuk membuat baris baru dalam output teks
+        //* \t digunakan untuk memberikan spasi 
+        //* \b digunakan untuk menghapus 1karakter/1huruf 
+        // " digunakan untuk mencetak karakter double quote
+        // ' digunakan untuk mencetak karakter singel quote
+        
+    }
+    
+}
