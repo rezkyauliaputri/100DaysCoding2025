6bb1d75cf4de842d82c193ca6381df3c92d3ab40
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-12 15:42:01 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,28 @@
+package day5.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        /* type data numerik bilangan bulat
+           yaitu byte long short int*/
+        //type data bilangan bulat
+        
+        byte umur = 19;
+        short tahun = 2006;
+        int kelas  = 36;
+        long uang_jajan = 10000;
+        
+        System.out.println("saya berumur " + umur);
+        System.out.println("saya lahir tahun " + tahun);
+        System.out.println("mahasiswa kelas c sebanyak " + kelas );
+        System.out.println("saya membawa uang saku sebanyak " + uang_jajan);
+        
+        //perbedaan type data
+        // type data diatas memiliki fungsi sama tetapi ukuran atau daya tampung berbeda
+        
+        
+        
+        
+    }
+    
+}
