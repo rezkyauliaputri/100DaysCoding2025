11cd0b5f4dc00948ef7d75f948b5b0412d59e7fd
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-13 15:58:05 +0800

    Day6.java

--- Day6.java
+++ Day6.java
@@ -6,11 +6,12 @@ public class Main {
         float berat = 49.0f;
         System.out.println("berat : " + berat + "kg");
         double tinggi = 165.0;
-        System.out.println("tinggi : " + tinggi + "cm"/*
+        System.out.println("tinggi : " + tinggi + "cm");
 
        /*perbedaan type data diatas ialah float memiliki kapasitas lebih kecil di banding double.
        float memiliki ukuran 32bit yang menyimpan angka desimal sekitar 7digit desimal
-sedangkan double memiliki ukuran lebih besar dibandingkan float ukuranya itu 64bit double menyimpan angka desimal  sekitar 15-16digit desimal/*
+       sedangkan double memiliki ukuran lebih besar dibandingkan float ukuranya itu 64bit 
+       double menyimpan angka desimal  sekitar 15-16digit desimaldesimal*/
          
         
     }
