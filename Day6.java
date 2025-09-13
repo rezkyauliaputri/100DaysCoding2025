ab6ba92b7e6d6698f8cd573473b6d7989165600b
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-13 13:52:42 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,17 @@
+package day6.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        float berat = 49f;
+        System.out.println("berat : " + berat + "kg");
+        double tinggi = 165;
+        System.out.println("tinggi : " + tinggi + "cm");
+        /*perbedaan tipe data diatas ialah float pengggunaan lebih
+         lebih kecil di banding double begitupun sebaliknya
+        double lebih besar dibandingkan float 
+         
+        
+    }
+    
+}
