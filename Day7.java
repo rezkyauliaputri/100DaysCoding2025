f8077efbbf86d4747fe236b39120afe0f1765826
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-14 10:08:45 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,20 @@
+package Day7.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        char gender = 'p';
+        boolean sehat = true;
+        System.out.println("gender : "+gender);
+        System.out.println("sehat : "+ sehat);
+        
+        /*perbedaan type data char dan boolean yaitu char menggunakan tanda 
+         petik ' dan menggunakan 1huruf saja sedangkan 
+        boolean itu hanya true dan false saja*/
+         
+    }
+}    
+  
+
+        
+        
