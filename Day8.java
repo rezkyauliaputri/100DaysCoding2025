24dabe2f27c479dc32ffaf5d7f395d442fd88b60
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-15 15:55:20 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,22 @@
+package Day8.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Rezky Aulia putri";
+        System.out.println(nama);
+        String nim = "D0225029";
+        System.out.println(nim);
+        String kelas = "c";
+        System.out.println(kelas);
+        String alamat = "Lakejo";
+        System.out.println(alamat);
+        String hobi = "volly";
+        System.out.println(hobi);
+        
+        // String ini menggunakan tanda kutip " 
+        // String termasuk pada type data non primitif
+        // String digunakan untuk kata atau kalimat 
+    }
+    
+}
