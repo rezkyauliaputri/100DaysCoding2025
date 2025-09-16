df7966506df0a73c3443e668b6721d089a873dc5
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-16 22:18:40 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,21 @@
+package Day9.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+     String nama;
+     //deklarasi adalah type data dan variabel yang digunakan
+     
+        nama = "rahmania";
+      String pekerjaan = "irt";
+     //inisialisasi yaitu pemberian variabel setelah = 
+        nama = "mahyuddin";
+       pekerjaan = "petani";
+     /*update variabel itu mengubah nilai variabel yang telah ada*/
+     
+     System.out.println(nama);
+     System.out.println(pekerjaan);
+        
+    }
+    
+}
