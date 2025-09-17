55f273198f78cfd34cdd1212075d9a849ef770aa
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-17 11:41:16 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,16 @@
+package Day10.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+         final double tinggi = 165.0;
+        System.out.println(tinggi);
+         
+        /* constanta (final variabel) itu nilai yang sudah
+         kita masukkan tidak bisa kita ubah atau update nilai variabelnya
+        contohnya jika kita menambah kan nilai lain maka itu bakal eror
+        karena final variabel itu tidak bisa mengupdate nilai variabel*/
+        
+    }
+    
+}
