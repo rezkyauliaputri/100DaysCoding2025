90545a9998399386abc60a1723f7f6ee83a02562
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-19 14:44:19 +0800

    Day12.java

---
+++ Day12.java
@@ -0,0 +1,29 @@
+package Day12.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // biodata saya 
+        String nama = "rezky aulia putri";
+        String nim = "D0225029";
+        int umur = 19;
+        char kelas = 'C';
+        String alamat ="Lakejo";
+        float berat = 48.0f;
+        double tinggi = 166.5;
+        // menampilkan output biodata 
+        System.out.println("nama \t\t: " + nama);
+        System.out.println("nim \t\t: " + nim);
+        System.out.println("umur \t\t: " + umur);
+        System.out.println("kelas \t\t: " + kelas + " informatika ");
+        System.out.println("alamat \t\t: " + alamat);
+        System.out.println("berat \t\t: " + berat);
+        System.out.println("tinggi \t\t: " + tinggi + " cm ");
+        //hari ini saya commit biodata saya menggunakan beberapa type data yaitu:
+        // String,int,double,float dan char
+        // \t\t untuk menyamaratakan =
+        
+        
+    }
+    
+}
