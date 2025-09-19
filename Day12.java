2cf9042871ca425a55549c50fa621e309e8f9e37
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-19 21:58:45 +0800

    Day12.java

--- Day12.java
+++ Day12.java
@@ -1,16 +1,31 @@
 package Day12.java;
+import java.util.Scanner;
 
 public class Main {
-    
+   
     public static void main(String[] args) {
         // biodata saya 
         String nama = "rezky aulia putri";
+        System.out.print("masukkan nama: ");
+        nama = input.nextline();
         String nim = "D0225029";
+        System.out.print(" masukkan nim: ");
+        nim = input.nextline();
         int umur = 19;
+        Syatem.out.print(" masukkan umur: ");
+        umur = input.nextint();
         char kelas = 'C';
+        Syatem.out.print(" masukkan kelas: ");
+        kelas = input.next(). charAt(0);
         String alamat ="Lakejo";
+        System.out.print(" masukkan alamat: ");
+        alamat = input.nextline();
         float berat = 48.0f;
+        Syatem.out.print(" masukkan berat: ");
+        berat = input. nextFloat();
         double tinggi = 166.5;
+        System.out.print("masukkan tinggi: ");
+        tinggi = input.nextDouble();
         // menampilkan output biodata 
         System.out.println("nama \t\t: " + nama);
         System.out.println("nim \t\t: " + nim);
