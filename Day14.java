1de1d51444d967675269d29f5178d955ededc482
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-21 20:40:13 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,45 @@
+
+package day14;
+public class Day14 {
+    public static void main(String[] args) {
+        
+        String aul = "6";
+        long fit = Long.parseLong(aul);
+        System.out.println(fit);
+        
+        String aul2 = "12";
+        byte imma = Byte.parseByte(aul2);
+        System.out.println(imma);
+        
+        String mama = "9";
+        int mamaaul = Integer.parseInt(mama);
+        System.out.println(mamaaul);
+        
+        String bapak = "2";
+        short bapakaul = Short.parseShort(bapak);
+        System.out.println(bapakaul);
+        
+        String adek = "5.7";
+        double adekaul = Double.parseDouble(adek);
+        System.out.println(adekaul);
+       
+        String pute = "7.2f";
+        float putri = Float.parseFloat(pute);
+        System.out.println(putri);
+        
+        String lia = "asliah";
+        char nuraslia = lia.charAt(1);
+        System.out.println(nuraslia);
+        
+        String rezkyauliaputri = "true";
+        boolean auliaputri = Boolean.parseBoolean(rezkyauliaputri);
+        System.out.println(auliaputri);
+        
+        
+        //mengubah type data non primitif (String) menjadi primitif yaitu byte,long,int,short,float,double,boolean,char
+        
+        
+       
+    }
+    
+}
