b40f48d2758c5a710cdc6b3ea9de1e8f09000e27
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-25 20:31:41 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,26 @@
+
+package day18;
+
+public class Day18 {
+
+    
+    public static void main(String[] args) {
+        
+       // konversi otomatis type data kecil ke besar
+       
+       //Wyde cast
+       byte peserta = 36;
+       int siswa = peserta;
+       System.out.println("byte ke int : " + siswa);
+       
+       short cowok = 68;
+       long cewek = cowok;
+        System.out.println("short ke long : " + cewek);
+        
+        
+        
+    }
+    
+    }
+    
+
