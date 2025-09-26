f6404bb4a1a6744bc1de17e7fb59cb01ceaa3db6
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-09-26 22:30:38 +0800

    Day19.java

--- Day19.java
+++ Day19.java
@@ -5,11 +5,11 @@ public class Day19 {
     public static void main(String[] args) {
         
         // Type cast 
-        float bb = 48;
+        float bb = 48.5f;
         int abc = (int)bb;
         System.out.println("float ke int : " + bb);
         
-        double tinggi = 168;
+        double tinggi = 168.2;
         byte kabel = (byte)tinggi;
         System.out.println("double ke byte : " + tinggi);
     }
