61469d27e11f251413a50d9c172c46c8e4a3bf8a
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-10-01 20:47:58 +0800

    Day24.java

--- Day24.java
+++ Day24.java
@@ -1,18 +1,20 @@
 
 package day24;
-
 import java.util.Scanner;
-
-public class Day24 {
-
+public class Main {
+    
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
-        // latihan program  luas lingkaran
-        System.out.print("masukkan jari jari lingkaran : ");
+        
+        System.out.print("masukkan jari jari lingkaran:");
         double r = n.nextDouble();
         
-        double luas = Math.PI * r * r;
-        System.out.println("luas lingkaran = " + luas);
+        final double h= 3.14;
+        double luas = h * r * r;
+        System.out.println("masukkan luas lingkaran:" + luas);
+        
+        
+        
         
     }
     
