ff952eda1ebc9f393fe3988a00dd44935536f4ec
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-10-02 20:59:11 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,24 @@
+package day25;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        
+    //operator increment dan decrement(++,--)
+
+        byte a = 4;
+        byte h = 8;
+        byte i =9;
+        byte j = 5;
+    
+        System.out.println(" post increment : a++ " + a);
+        System.out.println("pre increment : ++h " + h);
+        System.out.println("post decrement : i-- " + i);
+        System.out.println("pre decrement : --j " + j);
+            
+        
+        
+        
+    }
+    
+}
