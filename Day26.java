ea229b42e8c67447bf5c5d9f6d0e7c8b2fea6eda
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-10-03 20:10:51 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,26 @@
+
+package day26;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+        //operator penugasan 
+        int h,j;
+        h = 12;
+        j = 5;
+        
+        h += j;
+        System.out.println(h);
+        h -= j;
+        System.out.println(h);
+        h *= j;
+        System.out.println(h);
+        h /= j;
+        System.out.println(h);
+        h %= j;
+        System.out.println(h);
+        
+        
+    }
+    
+}
