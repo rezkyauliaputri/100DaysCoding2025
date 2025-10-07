0a736c4963d079f8eae9140ff65b4e6ac0c31377
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-10-07 20:19:35 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,22 @@
+
+package day30.java;
+
+import java.util.Scanner;
+
+public class Day30Java {
+
+   
+    public static void main(String[] args) {
+        Scanner aulia = new Scanner(System.in);
+        
+        System.out.print("nilai a : ");
+        int a = aulia.nextInt();
+        
+        System.out.print("nilai b : ");
+        int b = aulia.nextInt();
+        
+        System.out.println(a >= b); 
+        System.out.println(a <= b); 
+    }
+    
+}
