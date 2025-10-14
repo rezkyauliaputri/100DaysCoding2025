5d3b85f39c48e940914bf1eb7f2f115c02ef1506
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-10-14 19:57:57 +0800

    Day37.java

---
+++ Day37.java
@@ -0,0 +1,20 @@
+package Day37;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("nilai :");
+        int nilai = inp.nextInt();
+        if (nilai % 2 == 0){
+            System.out.println("nilai diatas merupakan nilai genap:");
+        }else{
+            System.out.println("nilai diatas merupakan nilai ganjil:");
+            
+        
+    }
+    
+}
+}
+    
+    
