e763ad9ca567fa7946ebb9847f68e421aa34f3cc
rezkyauliaputri <rezkyauliap17@gmail.com>
2025-10-20 20:01:18 +0800

    Day43.java

---
+++ Day43.java
@@ -0,0 +1,41 @@
+
+package day43;
+
+import java.util.Scanner;
+
+public class Day43 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Masukkan angka : ");
+        int angka = in.nextInt();
+        
+        if (angka < 10 && angka %2 == 0) {
+            System.out.print(angka + " adalah angka satuan genap");
+        }else if (angka < 10) {
+            System.out.print(angka + " adalah angka satuan ganjil");
+        }else if (angka >= 10 && angka <= 99 && angka %2 == 0) {
+            System.out.print(angka + " adalah angka puluhan genap");
+        }else if (angka >= 10 && angka <= 99) {
+            System.out.print(angka + " adalah angka puluhan ganjil");
+        }else if (angka >= 100 && angka <= 999 && angka %2 == 0) {
+            System.out.print(angka + " adalah angka ratusan genap");
+        }else if (angka >= 100 && angka <= 999) {
+            System.out.print(angka + " adalah angka ratusan ganjil");
+        }
+        if (angka %3 == 0 && angka %5 == 0) {
+            System.out.println(" yang bisa dibagi 3 dan 5");
+        }else if (angka %3 == 0) {
+            System.out.println(" yang bisa dibagi 3");
+        }else if (angka %5 == 0) {
+            System.out.println(" yang bisa dibagi 5");
+        }
+        
+    }
+    
+}
+
+    
+    
+
